4075c38361141db1e1abea03afbd1a5e7d6e895e
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-12-22 16:16:06 -0700

    disable component for now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { message } from './local.re'
-import LocalComponent from './LocalComponent'
+// import LocalComponent from './LocalComponent'
 
 ReactDOM.render(
   <div>
-    <LocalComponent name="friend" />
     <div>{message}</div>
   </div>,
   document.getElementById('root')
