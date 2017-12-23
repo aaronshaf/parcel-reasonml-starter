1ae205a2ba40aac161500a5b4ffb3566f52980f2
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-12-22 17:55:19 -0700

    include bs files; reenable component interop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { message } from './local.re'
-// import LocalComponent from './LocalComponent'
+import LocalComponent from './LocalComponent'
 
 ReactDOM.render(
   <div>
+    <LocalComponent name="friend" />
     <div>{message}</div>
   </div>,
   document.getElementById('root')
